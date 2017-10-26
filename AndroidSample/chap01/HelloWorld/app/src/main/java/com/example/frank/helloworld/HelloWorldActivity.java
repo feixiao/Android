89945132faeb5f8a99b15a8e423f2c8457849435
frank <feixiao2020@sina.com>
2017-10-26 21:33:42 +0800

    add androidsample

---
+++ AndroidSample/chap01/HelloWorld/app/src/main/java/com/example/frank/helloworld/HelloWorldActivity.java
@@ -0,0 +1,16 @@
+package com.example.frank.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class HelloWorldActivity extends AppCompatActivity {
+    private static final String TAG = "HelloWorldActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hello_world_layout);
+        Log.d(TAG,"onCreate......");
+    }
+}
---
+++ AndroidSample/chap02/activitytest/app/src/main/java/com/example/frank/activitytest/FirstActivity.java
@@ -0,0 +1,23 @@
+package com.example.frank.activitytest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+        Button btn = (Button) findViewById(R.id.btn1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(FirstActivity.this, "You clicked Button 1", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
