40b80656eef784fae3c5268df3a51b2d84828bfb
feixiao <feixiao2020@sina.com>
2022-07-11 10:52:58 +0800

    add

--- AppProgrammingSource/1.2/YoungHeart/app/src/main/java/com/youngheart/activity/others/MainActivity.java
+++ AppProgrammingSource/1.2/YoungHeart/app/src/main/java/com/youngheart/activity/others/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);		
+		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.activity_main);
 
