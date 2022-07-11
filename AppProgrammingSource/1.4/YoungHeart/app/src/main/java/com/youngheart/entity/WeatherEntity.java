db6104b25999bd3c10adb01eb12053843b4a3304
feixiao <feixiao2020@sina.com>
2022-07-11 14:08:26 +0800

    remove bom

--- AppProgrammingSource/1.4/YoungHeart/app/src/main/java/com/youngheart/entity/WeatherEntity.java
+++ AppProgrammingSource/1.4/YoungHeart/app/src/main/java/com/youngheart/entity/WeatherEntity.java
@@ -1,16 +1,13 @@
-﻿// JSON Java Class Generator
-// Written by Bruce Bao
-// Used for API: http://www.weather.com.cn/data/sk/101010100.html
 package com.youngheart.entity;
 
 public class WeatherEntity {
-	private WeatherInfo weatherinfo;
+    private WeatherInfo weatherinfo;
 
-	public WeatherInfo getWeatherInfo() {
-		return weatherinfo;
-	}
+    public WeatherInfo getWeatherInfo() {
+        return weatherinfo;
+    }
 
-	public void setWeatherInfo(WeatherInfo weatherinfo) {
-		this.weatherinfo = weatherinfo;
-	}
+    public void setWeatherInfo(WeatherInfo weatherinfo) {
+        this.weatherinfo = weatherinfo;
+    }
 }
