4d76cf3b30c3413db897afb4e29fe9acbe0baf33
feixiao <feixiao2020@sina.com>
2022-07-12 13:39:46 +0800

    update

--- AppProgrammingSource/1.3/YoungHeart/androidLib/src/main/java/com/infrastructure/activity/BaseActivity.java
+++ AppProgrammingSource/1.3/YoungHeart/androidLib/src/main/java/com/infrastructure/activity/BaseActivity.java
@@ -13,8 +13,12 @@ public abstract class BaseActivity extends Activity {
 		initViews(savedInstanceState);
 		loadData();
 	}
-	
+
+	// 子类分别实现
+	// initVariables ：初始化变量，包括 Intent 带的数据和 Activity 内的变量。
 	protected abstract void initVariables();
+	// initViews：加载 layout 布局文件，初始化控件，为控件挂上事件方法。
 	protected abstract void initViews(Bundle savedInstanceState);
+	// loadData：调用 MobileAPI 获取数据。
 	protected abstract void loadData();
 }
